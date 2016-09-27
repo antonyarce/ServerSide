603d88d0ba59a0780df05b4a09aa310dfa7c0dc4
Whiteeth <greekes@hotmail.com>
2016-09-27 17:16:32 -0600

    Cambios en JSONManager y cambio de biblioteca

--- app/src/main/java/com/androidsrc/server/JsonManager.java
+++ app/src/main/java/com/androidsrc/server/JsonManager.java
@@ -15,8 +15,26 @@ public class JsonManager {
         String dato;
         String respuesta="Aqui";
         String accion = parser.getString("Accion");
-        if(accion.equalsIgnoreCase("iniciar")){
-            respuesta="{\"Token:\""+ GenerateBase64.generar(GenerateUUID.crearUUID())+"\"}";
+        if(accion.equalsIgnoreCase("Conexion")){
+            String UUID = GenerateUUID.crearUUID();
+            String UUID64 = GenerateBase64.generar(UUID);
+            respuesta="{\"Token\":\""+UUID64+"\"}";
+            System.out.println("UUID:"+UUID);
+            System.out.println("UUID64:"+UUID64);
+        }if(accion.equalsIgnoreCase("xmalloc")){
+            String tokenrecibido = parser.getString("Token");
+            int size = parser.getInt("Size");
+        }if (accion.equalsIgnoreCase("xMalloc")){
+            String tokenrecibido = parser.getString("Token");
+            int size = parser.getInt("Size");
+            String datoguardar = parser.getString("Dato");
+        }if (accion.equalsIgnoreCase("xAssign")){
+            String tokenrecibido = parser.getString("Token");
+            String datoasignar = parser.getString("Dato");
+            String idEspacio = parser.getString("ID");
+        }if (accion.equalsIgnoreCase("xFree")){
+            String tokenrecibido = parser.getString("Token");
+            String idLiberar = parser.getString("ID");
         }
         System.out.println(accion);
         return respuesta;
